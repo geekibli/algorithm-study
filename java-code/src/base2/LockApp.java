24e562f3b841ac2441cc1e6a0e68737de988da2d
lei.gao <lei.gao@jiduauto.com>
2024-12-12 10:58:09 +0800

    savecode

---
+++ java-code/src/base2/LockApp.java
@@ -0,0 +1,12 @@
+package base2;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockApp {
+
+    public static void main(String[] args) {
+
+        ReentrantLock lock = new ReentrantLock();
+
+    }
+}
